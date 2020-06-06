019cec6d4a9b1ae8018ade4c675f165d965fd86f
Brianna Workman <brianna.workman@hotmail.com>
2020-06-06 11:04:52 -0400

    fixes cors issue

--- api/server.js
+++ api/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bcrypt = require('bcryptjs');
 const helmet = require('helmet');
+const cors = require('cors');
 
 const authenticate = require('../auth/authenticate-middleware.js');
 const authRouter = require('../auth/auth-router.js');
@@ -9,15 +10,15 @@ const usersRouter = require('../users/users-router.js');
 
 const server = express();
 
-server.get('/', (req, res) => {
+server.get('/', cors(), (req, res) => {
   res.status(200).json({ api: 'up', dbenv: process.env.DB_ENV });
 });
 
 server.use(helmet());
 server.use(express.json());
 
-server.use('/api/auth', authRouter);
-server.use('/api/plants', plantsRouter);
-server.use('/api/users', authenticate, usersRouter);
+server.use('/api/auth', cors(), authRouter);
+server.use('/api/plants', cors(), plantsRouter);
+server.use('/api/users', cors(), authenticate, usersRouter);
 
 module.exports = server;
