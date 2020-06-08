cbbb00d8c666db79dc5e12d4bdcc57eba65de0a6
Jasmine Kasheboon Khoury <jasminekh96@gmail.com>
2020-06-08 17:22:05 -0400

    fixed migration and seeds error

--- data/migrations/20200603223656_plant-table.js
+++ data/migrations/20200603223656_plant-table.js
@@ -1,12 +1,12 @@
-exports.up = function (knex) {
-  return knex.schema.createTable('plants', (plants) => {
-    plants.increments();
-    plants.string('name', 255).notNullable();
-    plants.string('image_URL').notNullable();
-    plants.string('water_frequency').notNullable();
-    plants.string('light');
-    plants.boolean('pet_friendly');
-  });
+exports.up = function(knex) {
+	return knex.schema.createTable('plants', (plants) => {
+		plants.increments();
+		plants.string('name', 255).notNullable();
+		plants.string('image_url').notNullable();
+		plants.string('water_frequency').notNullable();
+		plants.string('light');
+		plants.boolean('pet_friendly');
+	});
 };
 
-exports.down = function (knex) {};
+exports.down = function(knex) {};
